b6fad8e96b945582fd6603cff7faf0c2023945ba
Abubakr Abduvakhidov <abubakr@Abubakrs-MacBook-Pro.local>
2022-02-04 13:18:47 +0500

    Refactor; Add Container, Header components with styles; Use theme and styling props

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { StyledHeader } from "./styles/Header.styled";
+
+export default function Header() {
+    return (
+        <StyledHeader bg="red">
+            <h1>Hello world!</h1>
+        </StyledHeader>
+    );
+}
+    
