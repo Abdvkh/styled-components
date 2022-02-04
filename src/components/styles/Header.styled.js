d87001c49c2d01de80a1333e76b0c3f6526ecf14
Abubakr Abduvakhidov <abubakr@Abubakrs-MacBook-Pro.local>
2022-02-04 21:37:33 +0500

    Header section completed

--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -4,3 +4,31 @@ export const StyledHeader = styled.div`
     background-color: ${({ theme }) => theme.colors.header};
     padding: 40px 0;
 `;
+
+export const Nav = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 40px;
+
+    @media(max-width: ${({ theme }) => theme.mobile }){
+        flex-direction: column;
+    }
+`
+
+export const Logo = styled.img`
+    width: 100px;
+
+    @media(max-width: ${({ theme }) => theme.mobile }){
+        margin-bottom: 40px;
+    }
+`
+
+export const Image = styled.img`
+    width: 375px;
+    margin-left: 40px;
+
+    @media(max-width: ${({ theme }) => theme.mobile }){
+        margin: 40px 0 40px;
+    }
+`
